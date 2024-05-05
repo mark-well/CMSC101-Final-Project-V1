8bd3ae62b50bfececf52486e153a0606836115e2
mark-well <lacemerto05@gmail.com>
2024-05-06 02:40:16 +0800

    refactor: removed old questions, and added a new one

--- questions.js
+++ questions.js
@@ -2,26 +2,15 @@
     //All the questions
     let data = [
         {
-            question: "1 + 1 = ?",
-            optionA: "2",
-            optionB: "3",
-            optionC: "4",
-            optionD: "5",
-            correctAnswer: "A. 2",
+            question: "Filler",
+            optionA: "Filler",
+            optionB: "Filler",
+            optionC: "Filler",
+            optionD: "Filler",
+            correctAnswer: "A. Filler",
             alreadyAnswered: false,
             explanation: "N/A",
             _id: "NhNAaQJyYP241tHz"
-        },
-        {
-            question: "10 / 2",
-            optionA: "8",
-            optionB: "3",
-            optionC: "5",
-            optionD: "20",
-            correctAnswer: "C. 5",
-            alreadyAnswered: false,
-            explanation: "N/A",
-            _id: "Czw4c3yw1Ua8SRZk"
         }
     ];
 
