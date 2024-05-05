2ffda92e0eb3b7baa7d4ff3ac5409c67d012aefb
mark-well <lacemerto05@gmail.com>
2024-05-06 02:37:27 +0800

    I just forgot what i did here

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let nextButton = document.querySelector(".next-btn");
 let cards = slider.querySelectorAll(".card");
 let scoreDisplay = document.querySelector(".score-display");
 let selectAnswerErrorDisplay = document.querySelector(".select-answer-error");
-let remainingQuestionsDisplay = document.querySelector(".remaining-question-display");
+let remainingQuestionsDisplay = document.querySelector(
+    ".remaining-question-display"
+);
 let numberOfScoreCard = 0;
 let currentCard = 0;
 let userAnswer = "";
@@ -13,108 +15,101 @@ let score = savedScore != null ? Number(savedScore) : 0;
 let answeredQuestions = 0;
 
 //Load the questions from the database
-fetch("https://cardform.onrender.com/api")
-.then(res => res.json())
-.then(data => {
-	
-let dataFromDatabase = data; //This contains all the questions from the database
+let dataFromDatabase = questionsData.data; //This contains all the questions from the database
 let dataFromLocalstorage = JSON.parse(localStorage.getItem("data"));
 let original = [];
 let questions = [];
 
-if(dataFromLocalstorage != null) {
-	original = dataFromLocalstorage;
-	if(original.length != dataFromDatabase.length) {
-		if(original.length < dataFromDatabase.length)
-		{
-			console.log(original.length+1)
-			for(let i = original.length; i < dataFromDatabase.length; i++)
-			{
-				original.push(dataFromDatabase[i]);
-			}
-			console.log(original)
-		} else if(original.length > dataFromDatabase.length) {
-			original.splice(dataFromDatabase.length);
-		}
-		
-		localStorage.setItem("data", JSON.stringify(original));
-	}
+if (dataFromLocalstorage != null) {
+    original = dataFromLocalstorage;
+    if (original.length != dataFromDatabase.length) {
+        if (original.length < dataFromDatabase.length) {
+            for (let i = original.length; i < dataFromDatabase.length; i++) {
+                original.push(dataFromDatabase[i]);
+            }
+        } else if (original.length > dataFromDatabase.length) {
+            original.splice(dataFromDatabase.length);
+        }
+
+        localStorage.setItem("data", JSON.stringify(original));
+    }
 } else {
-	original = dataFromDatabase;
-	localStorage.setItem("data", JSON.stringify(dataFromDatabase));
+    original = dataFromDatabase;
+    localStorage.setItem("data", JSON.stringify(dataFromDatabase));
 }
 
 //Sound Effects
-var clickSfx = new Howl({src:['./sfx/click-sfx.wav']});
-var errorSfx = new Howl({src:['./sfx/error-sfx.wav']});
-var optionSelectionSfx = new Howl({src:['./sfx/option-selection-sfx.wav']});
+var clickSfx = new Howl({ src: ["./sfx/click-sfx.wav"] });
+var errorSfx = new Howl({ src: ["./sfx/error-sfx.wav"] });
+var optionSelectionSfx = new Howl({
+    src: ["./sfx/option-selection-sfx.wav"]
+});
 
 //Load all the questions
-for(let i = 0; i < original.length; i++) {
-	let item = original[i];
-	
-	if(item.alreadyAnswered) {
-		answeredQuestions += 1;
-		continue;
-	}
-	
-	questions.push(item);
+for (let i = 0; i < original.length; i++) {
+    let item = original[i];
+
+    if (item.alreadyAnswered) {
+        answeredQuestions += 1;
+        continue;
+    }
+
+    questions.push(item);
 }
 
-if(questions.length == 0) ScoreCard();
-for(let i = 0; i < questions.length; i++) {
-	let item = questions[i];
-	AddNewCard(item);
-	updateDisplay();
+if (questions.length == 0) ScoreCard();
+for (let i = 0; i < questions.length; i++) {
+    let item = questions[i];
+    AddNewCard(item);
+    updateDisplay();
 }
 
 updateDisplay();
 selectAnswer(); //Ask the user to select an answer first
 nextButton.addEventListener("click", () => {
-    let explanationWrapper = cards[currentCard].querySelector(".explanation-wrapper");
-    
+    let explanationWrapper = cards[currentCard].querySelector(
+        ".explanation-wrapper"
+    );
+
     //If user didn't select an answer
-    if(userAnswer == ""){
-    	selectAnswer();
-    	selectAnswerErrorDisplay.style.display = 'flex';
-    	errorSfx.play();
-    	//alert("Please select an answer");
-    }else{
-    	clickSfx.play();
-    	if(explanationWrapper.style.display == "block"){
-			
-			//Set the question to already answered
-			let cardId = cards[currentCard].dataset.id;
-    		let indexOfCurrentQuestion = original.indexOf(questions.find(item => item._id === cardId));
-    		original[indexOfCurrentQuestion].alreadyAnswered = true;
-    		localStorage.setItem("data", JSON.stringify(original));
-    		
-    		//Score Card (this makes sure that there's only one score card at the end)
-    		if(currentCard+1 == questions.length){
-    			numberOfScoreCard += 1;
-    			if(numberOfScoreCard <= 1) ScoreCard();
-    			scroll();
-    			return;
-    		}
-    		
-    		userAnswer = "";
-    		correctAnswer = "";
-    		currentCard += 1;
-    		selectAnswer();
-    		updateDisplay();
-    		scroll();
-    	} else {
-    		explanationWrapper.style.display = "block";
-    		checkAnswerIfCorrect();
-    		answeredQuestions += 1;
-    		updateDisplay();
-    	}
+    if (userAnswer == "") {
+        selectAnswer();
+        selectAnswerErrorDisplay.style.display = "flex";
+        errorSfx.play();
+        //alert("Please select an answer");
+    } else {
+        clickSfx.play();
+        if (explanationWrapper.style.display == "block") {
+            //Set the question to already answered
+            let cardId = cards[currentCard].dataset.id;
+            let indexOfCurrentQuestion = original.indexOf(
+                questions.find(item => item._id === cardId)
+            );
+            original[indexOfCurrentQuestion].alreadyAnswered = true;
+            localStorage.setItem("data", JSON.stringify(original));
+
+            //Score Card (this makes sure that there's only one score card at the end)
+            if (currentCard + 1 == questions.length) {
+                numberOfScoreCard += 1;
+                if (numberOfScoreCard <= 1) ScoreCard();
+                scroll();
+                return;
+            }
+
+            userAnswer = "";
+            correctAnswer = "";
+            currentCard += 1;
+            selectAnswer();
+            updateDisplay();
+            scroll();
+        } else {
+            explanationWrapper.style.display = "block";
+            checkAnswerIfCorrect();
+            answeredQuestions += 1;
+            updateDisplay();
+        }
     }
 });
-
-function closeSelectAnswerDisplay() {
-	selectAnswerErrorDisplay.style.display = "none";
-}
 
 //Scroll to the next card
 function scroll() {
@@ -131,40 +126,44 @@ function selectAnswer() {
     cards = slider.querySelectorAll(".card");
     options = cards[currentCard].querySelector(".options");
 
+    if (questions.length == 0) return;
     options.addEventListener("click", e => {
         const target = e.target;
         let selectedOption = options.querySelector(".selected");
         if (selectedOption) selectedOption.classList.remove("selected");
         target.classList.add("selected");
-    	
-    	userAnswer = options.querySelector(".selected").dataset.option;
-    	correctAnswer = questions[currentCard].correctAnswer[0].toUpperCase();
-    	optionSelectionSfx.play();
+
+        userAnswer = options.querySelector(".selected").dataset.option;
+        correctAnswer = questions[currentCard].correctAnswer[0].toUpperCase();
+        optionSelectionSfx.play();
     });
 }
 
-function checkAnswerIfCorrect(){
-	let explanationWrapper = cards[currentCard].querySelector(".explanation-wrapper");
-	if(userAnswer.toUpperCase() == correctAnswer) {
-		score += 1;
-		localStorage.setItem("score", score);
-		explanationWrapper.querySelector(".top-bar").style.backgroundColor = "#18ba59";
-	} else {
-		explanationWrapper.querySelector(".top-bar").style.backgroundColor = "#d62b2b";
-	}
-	
-	updateDisplay();
+function checkAnswerIfCorrect() {
+    let explanationWrapper = cards[currentCard].querySelector(
+        ".explanation-wrapper"
+    );
+    if (userAnswer.toUpperCase() == correctAnswer) {
+        score += 1;
+        localStorage.setItem("score", score);
+        explanationWrapper.querySelector(".top-bar").style.backgroundColor =
+            "#18ba59";
+    } else {
+        explanationWrapper.querySelector(".top-bar").style.backgroundColor =
+            "#d62b2b";
+    }
+
+    updateDisplay();
 }
 
 function updateDisplay() {
-	remainingQuestionsDisplay.innerText = `Questions: ${answeredQuestions}/${original.length}`;
-	scoreDisplay.innerText = `Total Score: ${score}`;
-	
+    remainingQuestionsDisplay.innerText = `Questions: ${answeredQuestions}/${original.length}`;
+    scoreDisplay.innerText = `Total Score: ${score}`;
 }
 
-function AddNewCard(item){
-	let n = "option" + item.correctAnswer.toUpperCase();
-	let text = `
+function AddNewCard(item) {
+    let n = "option" + item.correctAnswer.toUpperCase();
+    let text = `
 	<div class="card" data-id="${item._id}">
 		<div class="question">
 			<h4>Q. ${item.question}</h4>
@@ -190,12 +189,12 @@ function AddNewCard(item){
 			</div>
 		</div>
 	</div>`;
-	
-	slider.insertAdjacentHTML('beforeend', text);
+
+    slider.insertAdjacentHTML("beforeend", text);
 }
 
-function ScoreCard(){
-	let text = `
+function ScoreCard() {
+    let text = `
 		<div class="card score-card">
 			<div>
 				<p>Thankyou for Playing!</p>
@@ -206,12 +205,32 @@ function ScoreCard(){
 			</div>
 		</div>
 	`;
-	
-	slider.insertAdjacentHTML('beforeend', text);
+
+    slider.insertAdjacentHTML("beforeend", text);
 }
-});
+
+function FillerCard() {
+    let txt = `
+	<div class="card filler-card">
+		<h1>CARD FILLER</h1>
+		<br>
+		<p>If you're seeing this it only means that the <i>database server is currently down</i>,
+		we cannot retrieve the <i>data (questions)</i> for now.</p>
+		<br/>
+		<br>
+		<p>What to do?</p>
+		<div class="bar"></div>
+		<p>Better to contact the developer(s) to inform them about the issue.</p>
+	</div>`;
+
+    slider.insertAdjacentHTML("beforeend", txt);
+}
 
 function backToHome() {
-	window.location.href = "index.html";
-	clickSfx.play();
+    window.location.href = "index.html";
+    clickSfx.play();
+}
+
+function closeSelectAnswerDisplay() {
+    selectAnswerErrorDisplay.style.display = "none";
 }
